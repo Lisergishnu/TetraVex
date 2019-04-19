ce085b14a589eb12b50ee37eedc70a974bd1841f
Marco Benzi-Tobar <marco.benzi-tobar@inria.fr>
2019-04-19 16:28:50 +0200

    Added background color definition

--- TetraVex/TVPieceView.swift
+++ TetraVex/TVPieceView.swift
@@ -16,12 +16,19 @@ class TVPieceView : NSView, NSAccessibilityButton {
     var lastDraggedPosition : NSPoint = NSPoint()
     var controller : TVGameViewController?
     
+    @IBInspectable var shadowColor : NSColor = NSColor.secondarySelectedControlColor
+    @IBInspectable var backgroundColor : NSColor = NSColor(named: "Board Background Color")!
+    @IBInspectable var outerStrokeColor : NSColor =  NSColor.windowFrameTextColor
+    @IBInspectable var innerStrokeColor : NSColor = NSColor.windowFrameTextColor
+    @IBInspectable var textColor : NSColor = NSColor.labelColor
+    @IBInspectable var roundedRectRadius : CGFloat = 2
+    
     // MARK: - Dragging operations
     override func mouseDown(with event: NSEvent) {
         if (pieceModel?.boltedInPlace)! == false {
             isBeingDragged = true
             lastDraggedPosition = self.convert(event.locationInWindow, to: self)
-            let i = superview?.subviews.index(of: self)
+            let i = superview?.subviews.firstIndex(of: self)
             var svs = superview!.subviews
             if (i != svs.count-1) {
                 svs.swapAt(i!, svs.count-1)
@@ -67,8 +74,8 @@ class TVPieceView : NSView, NSAccessibilityButton {
     }
     
     // MARK: - Drawing operations
-
-    func drawStringCenteredAt(_ center: NSPoint, str: NSString, attribs: [NSAttributedStringKey: Any]?) {
+    
+    func drawStringCenteredAt(_ center: NSPoint, str: NSString, attribs: [NSAttributedString.Key: Any]?) {
         let b = str.boundingRect(with: NSSize(width: 300,height: 300), options: NSString.DrawingOptions.usesFontLeading, attributes: attribs)
         var dCenter = center
         dCenter.x = center.x - b.width/2
@@ -77,76 +84,73 @@ class TVPieceView : NSView, NSAccessibilityButton {
     }
     
     override func draw(_ dirtyRect: NSRect) {
-        if (pieceModel != nil) {
-            let pathRect = NSInsetRect(self.bounds, 5, 5)
-            let path = NSBezierPath(roundedRect: pathRect, xRadius: 2, yRadius: 2)
-            path.lineWidth = 2
-            
-            let shadow = NSShadow()
-            shadow.shadowBlurRadius = 1
-            
-            if isBeingDragged {
-                shadow.shadowColor = NSColor.secondarySelectedControlColor
-                shadow.shadowOffset = NSSize(width: 5, height: -5)
-                shadow.set()
-            }
-            
-            
-            NSColor.windowBackgroundColor.setFill()
-            NSColor.windowFrameTextColor.setStroke();
-            path.fill()
-            
-            NSGraphicsContext.saveGraphicsState()
-            NSColor.windowFrameColor.setStroke()
-            NSBezierPath.strokeLine(
-                from: pathRect.origin,
-                to: NSPoint(x: pathRect.maxX, y: pathRect.maxY)
-            )
-            NSBezierPath.strokeLine(
-                from: NSPoint(x:pathRect.minX,y:pathRect.maxY),
-                to: NSPoint(x: pathRect.maxX, y: pathRect.minY)
-            )
-            
-            NSGraphicsContext.restoreGraphicsState()
-            
-            path.stroke()
-            
-            let paragraphStyle : NSMutableParagraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.alignment = NSTextAlignment.center
-            
-            if #available(OSX 10.10, *) {
-                shadow.shadowColor = NSColor.tertiaryLabelColor
-            } else {
-                // Fallback on earlier versions
-            }
-            shadow.shadowOffset = NSSize(width: 1.5, height: -1.5)
-            shadow.shadowBlurRadius = 1
-            
-            let font = NSFont(name: "Helvetica", size: 18*(pathRect.height/80.0))
-            
-            let ptop = NSPoint(x: pathRect.width*0.5 + pathRect.minX,
-                               y: pathRect.height*0.80 + pathRect.minY)
-            let pbot = NSPoint(x: pathRect.width*0.5 + pathRect.minX,
-                               y: pathRect.height*0.20 + pathRect.minY)
-            let pleft = NSPoint(x: pathRect.width*0.20 + pathRect.minX,
-                                y: pathRect.height*0.5 + pathRect.minY)
-            let pright = NSPoint(x: pathRect.width*0.80 + pathRect.minX,
-                                 y: pathRect.height*0.5 + pathRect.minY)
-            
-            let attribs : [NSAttributedStringKey:Any] =
-                [NSAttributedStringKey.shadow:shadow,
-                 NSAttributedStringKey.font:font!,
-                 NSAttributedStringKey.strokeColor:NSColor.labelColor,
-                 NSAttributedStringKey.paragraphStyle:paragraphStyle]
-            var s = NSString(format: "%d", pieceModel!.topValue)
-            drawStringCenteredAt(ptop, str: s, attribs: attribs)
-            s = NSString(format: "%d", pieceModel!.bottomValue)
-            drawStringCenteredAt(pbot, str: s, attribs: attribs)
-            s = NSString(format: "%d", pieceModel!.leftValue)
-            drawStringCenteredAt(pleft, str: s, attribs: attribs)
-            s = NSString(format: "%d", pieceModel!.rightValue)
-            drawStringCenteredAt(pright, str: s, attribs: attribs)
+        guard let pieceModel = pieceModel else {
+            return
         }
+        
+        let pathRect = NSInsetRect(self.bounds, 5, 5)
+        let path = NSBezierPath(roundedRect: pathRect,
+                                xRadius: roundedRectRadius,
+                                yRadius: roundedRectRadius)
+        
+        // Background Fill
+        backgroundColor.setFill()
+        path.fill()
+        
+        // Inner X lines
+        NSGraphicsContext.saveGraphicsState()
+        innerStrokeColor.setStroke()
+        NSBezierPath.strokeLine(
+            from: pathRect.origin,
+            to: NSPoint(x: pathRect.maxX, y: pathRect.maxY)
+        )
+        NSBezierPath.strokeLine(
+            from: NSPoint(x:pathRect.minX,y:pathRect.maxY),
+            to: NSPoint(x: pathRect.maxX, y: pathRect.minY)
+        )
+        NSGraphicsContext.restoreGraphicsState()
+        
+        // Outer stroke
+        outerStrokeColor.setStroke();
+        path.lineWidth = 1
+        path.stroke()
+        
+        // Text
+        let paragraphStyle : NSMutableParagraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.center
+        let font = NSFont(name: "Helvetica", size: 18*(pathRect.height/80.0)) ?? NSFont.systemFont(ofSize: 18*(pathRect.height/80.0))
+        let ptop = NSPoint(x: pathRect.width*0.5 + pathRect.minX,
+                           y: pathRect.height*0.80 + pathRect.minY)
+        let pbot = NSPoint(x: pathRect.width*0.5 + pathRect.minX,
+                           y: pathRect.height*0.20 + pathRect.minY)
+        let pleft = NSPoint(x: pathRect.width*0.20 + pathRect.minX,
+                            y: pathRect.height*0.5 + pathRect.minY)
+        let pright = NSPoint(x: pathRect.width*0.80 + pathRect.minX,
+                             y: pathRect.height*0.5 + pathRect.minY)
+        let attribs : [NSAttributedString.Key:Any] =
+            [NSAttributedString.Key.font:font,
+             NSAttributedString.Key.strokeColor: textColor,
+             NSAttributedString.Key.paragraphStyle:paragraphStyle]
+        
+        var s = NSString(format: "%d", pieceModel.topValue)
+        drawStringCenteredAt(ptop, str: s, attribs: attribs)
+        s = NSString(format: "%d", pieceModel.bottomValue)
+        drawStringCenteredAt(pbot, str: s, attribs: attribs)
+        s = NSString(format: "%d", pieceModel.leftValue)
+        drawStringCenteredAt(pleft, str: s, attribs: attribs)
+        s = NSString(format: "%d", pieceModel.rightValue)
+        drawStringCenteredAt(pright, str: s, attribs: attribs)
+        
+        // Shadow
+        let shadow = NSShadow()
+        shadow.shadowBlurRadius = 1
+        shadow.shadowColor = shadowColor
+        if isBeingDragged {
+            shadow.shadowOffset = NSSize(width: 5, height: -5)
+        } else {
+            shadow.shadowOffset = NSSize(width: 1.5, height: -1.5)
+        }
+        shadow.set()
     }
     
     override func prepareForInterfaceBuilder() {
