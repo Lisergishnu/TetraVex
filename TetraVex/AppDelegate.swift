dfb7bafd64cf317d373046192b0e26872f55428d
Marco Benzi <marco.benzi-tobar@inria.fr>
2019-06-23 19:05:36 +0200

    Tab == 2 spaces

--- TetraVex/AppDelegate.swift
+++ TetraVex/AppDelegate.swift
@@ -11,23 +11,19 @@ import TetraVexKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    var currentGameModel : TVGameModel = TVGameModel()
-    var currentGamePieces : [[TVPieceModel]]?
-    
-    //MARK: - Resizing board
-    func setBoardSize(width: Int, height: Int) {
-        currentGameModel.boardWidth = width
-        currentGameModel.boardHeight = height
-    }
-    /*
-     * Reminder, First Responder actions have to marked with @IBAction
-     * to appear in the Interface Builder.
-     */
+
+  var currentGameModel : TVGameModel = TVGameModel()
+  var currentGamePieces : [[TVPieceModel]]?
 
   var optionMenu: NSMenuItem? {
     return NSApplication.shared.mainMenu?
       .item(withTitle: "Options")
+  }
+
+  //MARK: - Resizing board
+  func setBoardSize(width: Int, height: Int) {
+    currentGameModel.boardWidth = width
+    currentGameModel.boardHeight = height
   }
 
   // MARK: Board size menu manipulation
@@ -35,115 +31,115 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     return optionMenu?.submenu?.item(withTitle: "Size")
   }
 
-    @IBAction func setBoardTo2x2(sender: Any?) {
-        setBoardSize(width: 2, height: 2)
-        let sm = sizeSubMenu?.submenu
-        sm?.item(withTitle: "2x2")?.state = NSControl.StateValue(rawValue: 1)
-        sm?.item(withTitle: "3x3")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "4x4")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "5x5")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "6x6")?.state = NSControl.StateValue(rawValue: 0)
-    }
-    
-    @IBAction func setBoardTo3x3(sender: Any?) {
-        setBoardSize(width: 3, height: 3)
-        let sm = sizeSubMenu?.submenu
-        sm?.item(withTitle: "2x2")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "3x3")?.state = NSControl.StateValue(rawValue: 1)
-        sm?.item(withTitle: "4x4")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "5x5")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "6x6")?.state = NSControl.StateValue(rawValue: 0)
-    }
-    
-    @IBAction func setBoardTo4x4(sender: Any?) {
-        setBoardSize(width: 4, height: 4)
-        let sm = sizeSubMenu?.submenu
-        sm?.item(withTitle: "2x2")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "3x3")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "4x4")?.state = NSControl.StateValue(rawValue: 1)
-        sm?.item(withTitle: "5x5")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "6x6")?.state = NSControl.StateValue(rawValue: 0)
-    }
-    
-    @IBAction func setBoardTo5x5(sender: Any?) {
-        setBoardSize(width: 5, height: 5)
-        let sm = sizeSubMenu?.submenu
-        sm?.item(withTitle: "2x2")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "3x3")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "4x4")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "5x5")?.state = NSControl.StateValue(rawValue: 1)
-        sm?.item(withTitle: "6x6")?.state = NSControl.StateValue(rawValue: 0)
-    }
-    
-    @IBAction func setBoardTo6x6(sender: Any?) {
-        setBoardSize(width: 6, height: 6)
-        let sm = sizeSubMenu?.submenu
-        sm?.item(withTitle: "2x2")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "3x3")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "4x4")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "5x5")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "6x6")?.state = NSControl.StateValue(rawValue: 1)
-    }
+  @IBAction func setBoardTo2x2(sender: Any?) {
+    setBoardSize(width: 2, height: 2)
+    let sm = sizeSubMenu?.submenu
+    sm?.item(withTitle: "2x2")?.state = NSControl.StateValue(rawValue: 1)
+    sm?.item(withTitle: "3x3")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "4x4")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "5x5")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "6x6")?.state = NSControl.StateValue(rawValue: 0)
+  }
+
+  @IBAction func setBoardTo3x3(sender: Any?) {
+    setBoardSize(width: 3, height: 3)
+    let sm = sizeSubMenu?.submenu
+    sm?.item(withTitle: "2x2")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "3x3")?.state = NSControl.StateValue(rawValue: 1)
+    sm?.item(withTitle: "4x4")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "5x5")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "6x6")?.state = NSControl.StateValue(rawValue: 0)
+  }
+
+  @IBAction func setBoardTo4x4(sender: Any?) {
+    setBoardSize(width: 4, height: 4)
+    let sm = sizeSubMenu?.submenu
+    sm?.item(withTitle: "2x2")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "3x3")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "4x4")?.state = NSControl.StateValue(rawValue: 1)
+    sm?.item(withTitle: "5x5")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "6x6")?.state = NSControl.StateValue(rawValue: 0)
+  }
+
+  @IBAction func setBoardTo5x5(sender: Any?) {
+    setBoardSize(width: 5, height: 5)
+    let sm = sizeSubMenu?.submenu
+    sm?.item(withTitle: "2x2")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "3x3")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "4x4")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "5x5")?.state = NSControl.StateValue(rawValue: 1)
+    sm?.item(withTitle: "6x6")?.state = NSControl.StateValue(rawValue: 0)
+  }
+
+  @IBAction func setBoardTo6x6(sender: Any?) {
+    setBoardSize(width: 6, height: 6)
+    let sm = sizeSubMenu?.submenu
+    sm?.item(withTitle: "2x2")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "3x3")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "4x4")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "5x5")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "6x6")?.state = NSControl.StateValue(rawValue: 1)
+  }
 
 
-    //MARK: - Changing range of digits
-    func setNumberOfDigits(num :Int) {
-        currentGameModel.currentNumberDigits = num
-    }
+  //MARK: - Changing range of digits
+  func setNumberOfDigits(num :Int) {
+    currentGameModel.currentNumberDigits = num
+  }
 
-    // MARK: Number of digits menu manipulation
+  // MARK: Number of digits menu manipulation
   var numberOfDigitsSubMenu: NSMenuItem? {
     return optionMenu?.submenu?.item(withTitle: "Digits")
   }
-    @IBAction func setNumberOfDigitsTo6(sender: Any?) {
-        setNumberOfDigits(num: 5)
-        let sm = numberOfDigitsSubMenu?.submenu
-        sm?.item(withTitle: "6")?.state = NSControl.StateValue(rawValue: 1)
-        sm?.item(withTitle: "7")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "8")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "9")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "10")?.state = NSControl.StateValue(rawValue: 0)
-    }
-    
-    @IBAction func setNumberOfDigitsTo7(sender: Any?) {
-        setNumberOfDigits(num: 6)
-        let sm = numberOfDigitsSubMenu?.submenu
-        sm?.item(withTitle: "6")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "7")?.state = NSControl.StateValue(rawValue: 1)
-        sm?.item(withTitle: "8")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "9")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "10")?.state = NSControl.StateValue(rawValue: 0)
-    }
-    
-    @IBAction func setNumberOfDigitsTo8(sender: Any?) {
-        setNumberOfDigits(num: 7)
-        let sm = numberOfDigitsSubMenu?.submenu
-        sm?.item(withTitle: "6")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "7")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "8")?.state = NSControl.StateValue(rawValue: 1)
-        sm?.item(withTitle: "9")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "10")?.state = NSControl.StateValue(rawValue: 0)
-    }
+  @IBAction func setNumberOfDigitsTo6(sender: Any?) {
+    setNumberOfDigits(num: 5)
+    let sm = numberOfDigitsSubMenu?.submenu
+    sm?.item(withTitle: "6")?.state = NSControl.StateValue(rawValue: 1)
+    sm?.item(withTitle: "7")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "8")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "9")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "10")?.state = NSControl.StateValue(rawValue: 0)
+  }
 
-    @IBAction func setNumberOfDigitsTo9(sender: Any?) {
-        setNumberOfDigits(num: 8)
-        let sm = numberOfDigitsSubMenu?.submenu
-        sm?.item(withTitle: "6")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "7")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "8")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "9")?.state = NSControl.StateValue(rawValue: 1)
-        sm?.item(withTitle: "10")?.state = NSControl.StateValue(rawValue: 0)
-    }
-    
-    @IBAction func setNumberOfDigitsTo10(sender: Any?) {
-        setNumberOfDigits(num: 9)
-        let sm = numberOfDigitsSubMenu?.submenu
-        sm?.item(withTitle: "6")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "7")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "8")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "9")?.state = NSControl.StateValue(rawValue: 0)
-        sm?.item(withTitle: "10")?.state = NSControl.StateValue(rawValue: 1)
-    }
+  @IBAction func setNumberOfDigitsTo7(sender: Any?) {
+    setNumberOfDigits(num: 6)
+    let sm = numberOfDigitsSubMenu?.submenu
+    sm?.item(withTitle: "6")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "7")?.state = NSControl.StateValue(rawValue: 1)
+    sm?.item(withTitle: "8")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "9")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "10")?.state = NSControl.StateValue(rawValue: 0)
+  }
+
+  @IBAction func setNumberOfDigitsTo8(sender: Any?) {
+    setNumberOfDigits(num: 7)
+    let sm = numberOfDigitsSubMenu?.submenu
+    sm?.item(withTitle: "6")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "7")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "8")?.state = NSControl.StateValue(rawValue: 1)
+    sm?.item(withTitle: "9")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "10")?.state = NSControl.StateValue(rawValue: 0)
+  }
+
+  @IBAction func setNumberOfDigitsTo9(sender: Any?) {
+    setNumberOfDigits(num: 8)
+    let sm = numberOfDigitsSubMenu?.submenu
+    sm?.item(withTitle: "6")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "7")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "8")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "9")?.state = NSControl.StateValue(rawValue: 1)
+    sm?.item(withTitle: "10")?.state = NSControl.StateValue(rawValue: 0)
+  }
+
+  @IBAction func setNumberOfDigitsTo10(sender: Any?) {
+    setNumberOfDigits(num: 9)
+    let sm = numberOfDigitsSubMenu?.submenu
+    sm?.item(withTitle: "6")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "7")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "8")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "9")?.state = NSControl.StateValue(rawValue: 0)
+    sm?.item(withTitle: "10")?.state = NSControl.StateValue(rawValue: 1)
+  }
 
 
   @IBAction func setTextStyleToDigits(sender: Any?) {
@@ -180,15 +176,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     controller.setTextStyle(to: .greekSymbols)
   }
-    
-    //MARK: - Game actions
-    @IBAction func newGame(sender: Any?) {
-        let pg = TVPuzzleGenerator(width: currentGameModel.boardWidth, height: currentGameModel.boardHeight, rangeOfNumbers: 0...currentGameModel.currentNumberDigits)
-        currentGamePieces = pg.solvedBoard
-        let pv : TVGameViewController = NSApplication.shared.mainWindow?.contentViewController as! TVGameViewController
-        pv.solvedBoard = currentGamePieces
-        pv.newBoard(currentGameModel.boardWidth, height: currentGameModel.boardHeight)
-        
-    }
-	
+
+  //MARK: - Game actions
+  @IBAction func newGame(sender: Any?) {
+    let pg = TVPuzzleGenerator(width: currentGameModel.boardWidth, height: currentGameModel.boardHeight, rangeOfNumbers: 0...currentGameModel.currentNumberDigits)
+    currentGamePieces = pg.solvedBoard
+    let pv : TVGameViewController = NSApplication.shared.mainWindow?.contentViewController as! TVGameViewController
+    pv.solvedBoard = currentGamePieces
+    pv.newBoard(currentGameModel.boardWidth, height: currentGameModel.boardHeight)
+
+  }
+
 }
--- TetraVex/TVGameViewController.swift
+++ TetraVex/TVGameViewController.swift
@@ -27,164 +27,164 @@ import GameplayKit
 
 class TVGameViewController: NSViewController
 {
-	var solvedBoard : [[TVPieceModel]]? = nil
-	@IBOutlet weak var boardAreaBox: TVBoardView!
-	@IBOutlet weak var templatePieceView: TVPieceView!
-	var currentPiecesOnBoard : [TVPieceView] = []
-	var boardModel: TVBoardModel?
-	var delegate : AppDelegate?
-    @IBOutlet weak var boardHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var boardWidthConstraint: NSLayoutConstraint!
+  var solvedBoard : [[TVPieceModel]]? = nil
+  @IBOutlet weak var boardAreaBox: TVBoardView!
+  @IBOutlet weak var templatePieceView: TVPieceView!
+  var currentPiecesOnBoard : [TVPieceView] = []
+  var boardModel: TVBoardModel?
+  var delegate : AppDelegate?
+  @IBOutlet weak var boardHeightConstraint: NSLayoutConstraint!
+  @IBOutlet weak var boardWidthConstraint: NSLayoutConstraint!
+  
+  //timing
+  var timer: Timer?
+  var secondsPassed: Int = 0
+  @IBOutlet weak var timerLabel: NSTextField!
+  
+  //scores
+  var scores: TVHighScores?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    delegate = NSApplication.shared.delegate as? AppDelegate
+    scores = TVHighScores.read()
+  }
+  
+  override var representedObject: Any? {
+    didSet {
+      // Update the view, if already loaded.
+    }
+  }
+  
+  func newBoard(_ width: Int, height: Int) {
+    boardModel = TVBoardModel(width: width, height: height)
     
-	//timing
-	var timer: Timer?
-	var secondsPassed: Int = 0
-	@IBOutlet weak var timerLabel: NSTextField!
-
-	//scores
-	var scores: TVHighScores?
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-		delegate = NSApplication.shared.delegate as? AppDelegate
-		scores = TVHighScores.read()
-	}
-
-	override var representedObject: Any? {
-		didSet {
-			// Update the view, if already loaded.
-		}
-	}
-
-	func newBoard(_ width: Int, height: Int) {
-		boardModel = TVBoardModel(width: width, height: height)
-
-        // Refresh autolayout
-        let pw  = templatePieceView.frame.width
-        let ph  = templatePieceView.frame.height
-
-        boardWidthConstraint.constant = pw*CGFloat(width)
-        boardHeightConstraint.constant = ph*CGFloat(height)
-        
-		/* Generate and shuffle new pieces */
-		/* Also delete previous pieces */
-		for pv in currentPiecesOnBoard {
-			pv.removeFromSuperview()
-		}
-		if ((solvedBoard) != nil) {
+    // Refresh autolayout
+    let pw  = templatePieceView.frame.width
+    let ph  = templatePieceView.frame.height
+    
+    boardWidthConstraint.constant = pw*CGFloat(width)
+    boardHeightConstraint.constant = ph*CGFloat(height)
+    
+    /* Generate and shuffle new pieces */
+    /* Also delete previous pieces */
+    for pv in currentPiecesOnBoard {
+      pv.removeFromSuperview()
+    }
+    if ((solvedBoard) != nil) {
       visiblePieces = [TVPieceView]()
-			for i in 0..<width {
-				for j in 0..<height {
-					let nfr = templatePieceView.frame.offsetBy(dx: pw*CGFloat(i), dy: -ph*CGFloat(j))
-					let pv : TVPieceView = TVPieceView(frame: nfr)
-					pv.autoresizingMask = [NSView.AutoresizingMask.maxXMargin, NSView.AutoresizingMask.minYMargin]
-					currentPiecesOnBoard.append(pv)
-					pv.pieceModel = solvedBoard![i][j]
-                    pv.delegate = self
-					self.view.addSubview(pv)
+      for i in 0..<width {
+        for j in 0..<height {
+          let nfr = templatePieceView.frame.offsetBy(dx: pw*CGFloat(i), dy: -ph*CGFloat(j))
+          let pv : TVPieceView = TVPieceView(frame: nfr)
+          pv.autoresizingMask = [NSView.AutoresizingMask.maxXMargin, NSView.AutoresizingMask.minYMargin]
+          currentPiecesOnBoard.append(pv)
+          pv.pieceModel = solvedBoard![i][j]
+          pv.delegate = self
+          self.view.addSubview(pv)
           visiblePieces?.append(pv)
-				}
-			}
-
-		}
-
-		//start timing the game
-		if timer != nil {
-			timer?.invalidate()
-			timer = nil
-		}
-		secondsPassed = 0
-		timer = Timer.scheduledTimer(
-			timeInterval: 1,
-			target: self,
-			selector: #selector(tick),
-			userInfo: nil,
-			repeats: true)
-	}
-
-	func removeFromBoard(piece p:TVPieceView) -> Bool {
-		if boardModel!.removePieceFromBoard(p.pieceModel!) {
-			p.pieceModel!.isOnBoard = false
-			return true
-		}
-		return false
-	}
-
-	func checkPiece(with pv:TVPieceView,at dropOffPosition:NSPoint) {
-		/* Determine the position of view inside the grid */
-		if boardAreaBox.frame.contains(dropOffPosition) {
-			let i : Int = Int((dropOffPosition.x - boardAreaBox.frame.origin.x) / pv.frame.width)
-			let j : Int = Int((dropOffPosition.y - boardAreaBox.frame.origin.y) / pv.frame.height)
-
-			if boardModel!.addPieceToBoard(pv.pieceModel!, x: i, y: j) {
-				pv.frame.origin.x = CGFloat(i)*pv.frame.width + boardAreaBox.frame.origin.x
-				pv.frame.origin.y = CGFloat(j)*pv.frame.height + boardAreaBox.frame.origin.y
-				pv.pieceModel?.isOnBoard = true
-				if boardModel!.isCompleted() {
-					timer?.invalidate()
-					timer = nil
-
-					let size = "\(boardModel!.boardWidth)x\(boardModel!.boardHeight)"
-					scores?.scores?[size]?[NSDate()] = secondsPassed
-					scores?.save()
-				}
-			} else {
-                let randomSource = GKARC4RandomSource()
-                var newOrigin = templatePieceView.frame.origin
-                newOrigin.x +=  CGFloat(Int.random(0...100,randomSource: randomSource))
-                newOrigin.y -=  CGFloat(Int.random(0...100,randomSource: randomSource))
-                pv.animator().setFrameOrigin(newOrigin)
-			}
-		}
-	}
-
-	//MARK: - Timing
-	@objc func tick() {
-		secondsPassed += 1
-		timerLabel.stringValue = TVHighScores.timeToString(secondsPassed)
-	}
-
+        }
+      }
+      
+    }
+    
+    //start timing the game
+    if timer != nil {
+      timer?.invalidate()
+      timer = nil
+    }
+    secondsPassed = 0
+    timer = Timer.scheduledTimer(
+      timeInterval: 1,
+      target: self,
+      selector: #selector(tick),
+      userInfo: nil,
+      repeats: true)
+  }
+  
+  func removeFromBoard(piece p:TVPieceView) -> Bool {
+    if boardModel!.removePieceFromBoard(p.pieceModel!) {
+      p.pieceModel!.isOnBoard = false
+      return true
+    }
+    return false
+  }
+  
+  func checkPiece(with pv:TVPieceView,at dropOffPosition:NSPoint) {
+    /* Determine the position of view inside the grid */
+    if boardAreaBox.frame.contains(dropOffPosition) {
+      let i : Int = Int((dropOffPosition.x - boardAreaBox.frame.origin.x) / pv.frame.width)
+      let j : Int = Int((dropOffPosition.y - boardAreaBox.frame.origin.y) / pv.frame.height)
+      
+      if boardModel!.addPieceToBoard(pv.pieceModel!, x: i, y: j) {
+        pv.frame.origin.x = CGFloat(i)*pv.frame.width + boardAreaBox.frame.origin.x
+        pv.frame.origin.y = CGFloat(j)*pv.frame.height + boardAreaBox.frame.origin.y
+        pv.pieceModel?.isOnBoard = true
+        if boardModel!.isCompleted() {
+          timer?.invalidate()
+          timer = nil
+          
+          let size = "\(boardModel!.boardWidth)x\(boardModel!.boardHeight)"
+          scores?.scores?[size]?[NSDate()] = secondsPassed
+          scores?.save()
+        }
+      } else {
+        let randomSource = GKARC4RandomSource()
+        var newOrigin = templatePieceView.frame.origin
+        newOrigin.x +=  CGFloat(Int.random(0...100,randomSource: randomSource))
+        newOrigin.y -=  CGFloat(Int.random(0...100,randomSource: randomSource))
+        pv.animator().setFrameOrigin(newOrigin)
+      }
+    }
+  }
+  
+  //MARK: - Timing
+  @objc func tick() {
+    secondsPassed += 1
+    timerLabel.stringValue = TVHighScores.timeToString(secondsPassed)
+  }
+  
   // MARK: Changing the piece text style
   var visiblePieces : [TVPieceView]?
-
+  
   func setTextStyle(to style:TVPieceModel.TextStyle) {
     guard let pieces = visiblePieces else {
       return
     }
     textStyle(for: pieces, style)
   }
-
+  
   func textStyle(for pieces:[TVPieceView],_ style: TVPieceModel.TextStyle) {
     for piece in pieces {
       guard var model = piece.pieceModel else {
         continue
       }
-        model.textStyle = style
-        piece.pieceModel = model
-        piece.needsDisplay = true
+      model.textStyle = style
+      piece.pieceModel = model
+      piece.needsDisplay = true
     }
   }
-    
+  
 }
 
 extension TVGameViewController : TVPieceViewDelegate {
-    func wasLiftedFromBoard(piece: TVPieceView) {
-        removeFromBoard(piece: piece)
-        guard let layer = piece.layer else {
-            return
-        }
-        layer.shadowColor = NSColor.black.cgColor
-        layer.shadowOpacity = 0.75
-        layer.shadowOffset = CGSize(width: 5,height: 10)
-        layer.shadowRadius = 10
+  func wasLiftedFromBoard(piece: TVPieceView) {
+    removeFromBoard(piece: piece)
+    guard let layer = piece.layer else {
+      return
     }
-    
-    func wasDropped(piece: TVPieceView, at: NSPoint) {
-        checkPiece(with: piece, at: at)
-        guard let layer = piece.layer else {
-            return
-        }
-        layer.shadowOpacity = 0.0
+    layer.shadowColor = NSColor.black.cgColor
+    layer.shadowOpacity = 0.75
+    layer.shadowOffset = CGSize(width: 5,height: 10)
+    layer.shadowRadius = 10
+  }
+  
+  func wasDropped(piece: TVPieceView, at: NSPoint) {
+    checkPiece(with: piece, at: at)
+    guard let layer = piece.layer else {
+      return
     }
+    layer.shadowOpacity = 0.0
+  }
 }
